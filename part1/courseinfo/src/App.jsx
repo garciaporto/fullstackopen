53f3eefb5b8e5bf2896a69ba66a008f0f075199d
garciaporto <garciaporto@gmail.com>
2025-05-01 10:40:12 +0200

    Exersises 1.3 - 1.4

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,55 @@
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.course}</h1>
+    </>
+  );
+};
+
+const Part = (props) => {
+  console.log(props);
+  return (
+    <>
+      <p>
+        {props.part} {props.exercises}
+      </p>
+    </>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      <Part part={parts[0].name} exercises={parts[0].exercises} />
+      <Part part={parts[1].name} exercises={parts[1].exercises} />
+      <Part part={parts[2].name} exercises={parts[2].exercises} />
+    </>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <>
+      <p>Number of exercises {props.total}</p>
+    </>
+  );
+};
+
+const App = () => {
+  const course = "Half Stack application development";
+  const parts = [
+    { name: "Fundamentals of React", exercises: 10 },
+    { name: "Using props to pass data", exercises: 7 },
+    { name: "State of a component", exercises: 14 },
+  ];
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total total={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
+    </div>
+  );
+};
+
+export default App;
