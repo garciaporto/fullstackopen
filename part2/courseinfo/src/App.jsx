dcbd5d5c042f622446215ba4464060d7d5b2ed33
garciaporto <garciaporto@gmail.com>
2025-05-12 21:54:22 +0200

    courseinfo: exercises 2.1 to 2.3

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,64 @@
+const Course = (props) => {
+  return (
+    <div>
+      <Header course={props.course.name} />
+      <Content parts={props.course.parts} />
+      <Total parts={props.course.parts} />
+    </div>
+  );
+};
+
+const Header = ({ course }) => (
+  <>
+    <h1>{course}</h1>
+  </>
+);
+
+const Content = (props) => (
+  <div>
+    {props.parts.map((item) => (
+      <Part part={item} key={item.id} />
+    ))}
+  </div>
+);
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+);
+
+const Total = (props) => {
+  let sum = props.parts.reduce((acc, item) => {
+    return (acc += item.exercises);
+  }, 0);
+  return <p>Number of exercises {sum}</p>;
+};
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
+
+export default App;
