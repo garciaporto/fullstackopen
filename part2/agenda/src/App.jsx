28f04d9ebc6affad3748b2872694c58f462f23bc
garciaporto <garciaporto@gmail.com>
2025-05-29 21:40:53 +0200

    Agenda: exercise 2.17

--- part2/agenda/src/App.jsx
+++ part2/agenda/src/App.jsx
@@ -3,12 +3,16 @@ import numberService from "./services/registration";
 import PersonForm from "./components/PersonForm";
 import Filter from "./components/Filter";
 import Persons from "./components/Persons";
+import Notification from "./components/Notification";
+import "./index.css";
 
 const App = () => {
   const [newName, setNewName] = useState("");
   const [newPhone, setNewPhone] = useState("");
   const [search, setSearch] = useState("");
   const [persons, setPersons] = useState([]);
+  const [alertMessage, setAlertMessage] = useState(null);
+  const [alertStyle, setAlertStyle] = useState("");
 
   useEffect(() => {
     numberService.getAll().then((list) => setPersons(list));
@@ -43,9 +47,20 @@ const App = () => {
             setNewName("");
             setNewPhone("");
             setSearch("");
+            setAlertMessage("Contact data succesfully updated!");
+            setAlertStyle("alert success");
+            setTimeout(() => {
+              setAlertMessage(null);
+              setAlertStyle("");
+            }, 3000);
           })
           .catch(() => {
-            alert(`The contact was already deleted from the server`);
+            setAlertMessage("The contact was already deleted from the server");
+            setAlertStyle("alert error");
+            setTimeout(() => {
+              setAlertMessage(null);
+              setAlertStyle("");
+            }, 3000);
             setPersons(persons.filter((person) => person.id !== updatedContact.id));
           });
       }
@@ -56,6 +71,12 @@ const App = () => {
     numberService.create(contactObject).then((returnedPerson) => setPersons(persons.concat(returnedPerson)));
     setNewName("");
     setNewPhone("");
+    setAlertMessage("A new contact has been created!");
+    setAlertStyle("alert success");
+    setTimeout(() => {
+      setAlertMessage(null);
+      setAlertStyle("");
+    }, 3000);
   };
 
   const removeName = (id) => {
@@ -63,9 +84,28 @@ const App = () => {
     const deleteConfirmation = window.confirm(`Delete ${nameToRemove.name}?`);
 
     if (deleteConfirmation) {
-      numberService.remove(id).then(() => {
-        setPersons(persons.filter((person) => person.id !== id));
-      });
+      numberService
+        .remove(id)
+        .then(() => {
+          setAlertMessage(`${nameToRemove.name} has been deleted`);
+          setAlertStyle("alert success");
+          setPersons(persons.filter((person) => person.id !== id));
+          setTimeout(() => {
+            setAlertMessage(null);
+            setAlertStyle("");
+          }, 3000);
+        })
+        .catch((error) => {
+          setAlertMessage(error.message);
+          setAlertStyle("alert error");
+          setNewName("");
+          setNewPhone("");
+          setPersons(persons.filter((person) => person.id !== id));
+          setTimeout(() => {
+            setAlertMessage(null);
+            setAlertStyle("");
+          }, 3000);
+        });
     }
   };
 
@@ -76,6 +116,7 @@ const App = () => {
   return (
     <div>
       <h1>Phonebook</h1>
+      <Notification notificationClass={alertStyle} message={alertMessage} />
       <Filter searchControl={search} onChange={handleSearch} />
       <h2>Add new</h2>
       <PersonForm onSubmit={addName} onChangeName={handleNameChange} onChangePhone={handlePhoneChange} nameControl={newName} phoneControl={newPhone} />
---
+++ part2/agenda/src/components/Notification.jsx
@@ -0,0 +1,6 @@
+const Notification = ({ message, notificationClass }) => {
+  if (message === null) return null;
+  return <div className={notificationClass}>{message}</div>;
+};
+
+export default Notification;
